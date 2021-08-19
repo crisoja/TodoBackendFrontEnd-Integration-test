ad82c263188a4f0d00adda38dc9f6473a4f3109d
Janley Crsiologo <crisoja@oocl.com>
2021-08-19 14:21:12 +0800

    [FrontEnd-BackEnd-IntegrationTest][Janley] feat: implement CorsGlobalConfiguration

--- src/main/java/com/example/todolist/config/CorsGlobalConfiguration.java
+++ src/main/java/com/example/todolist/config/CorsGlobalConfiguration.java
@@ -2,13 +2,20 @@ package com.example.todolist.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
 public class CorsGlobalConfiguration {
 
-//    @Bean
-//    WebMvcConfigurer corsConfiguration(){
-//
-//    }
+    @Bean
+    WebMvcConfigurer corsConfiguration(){
+        return new WebMvcConfigurer(){
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**")
+                        .allowedOrigins("Http://localhost:3000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
+    }
 }
--- src/main/java/com/example/todolist/controller/TodoController.java
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins = "Http://localhost:3000")
 @RestController
 @RequestMapping("/todos")
 public class TodoController {
