d735aa78e0d92e4a5e727f47df18ecc8bc208642
Janley Crsiologo <crisoja@oocl.com>
2021-08-19 19:33:07 +0800

    [FrontEnd-BackEnd-IntegrationTest][Janley] refactor: remove extra spaces

--- src/main/java/com/example/todolist/controller/TodoController.java
+++ src/main/java/com/example/todolist/controller/TodoController.java
@@ -1,6 +1,5 @@
 package com.example.todolist.controller;
 
-
 import com.example.todolist.model.Todo;
 import com.example.todolist.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,34 +15,33 @@ public class TodoController {
     private final TodoService todoService;
 
     @Autowired
-    public TodoController(TodoService todoService){
+    public TodoController(TodoService todoService) {
         this.todoService = todoService;
     }
 
     @GetMapping
-    public List<Todo> findTodos(){
+    public List<Todo> findTodos() {
         return todoService.findTodos();
     }
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Todo addTodo(@RequestBody Todo todo){
+    public Todo addTodo(@RequestBody Todo todo) {
         return todoService.addTodo(todo);
     }
 
     @GetMapping("/{id}")
-    public Todo findTodoById(@PathVariable Integer id){
+    public Todo findTodoById(@PathVariable Integer id) {
         return todoService.findTodoById(id);
     }
 
     @PutMapping("/{id}")
-    public Todo updateTodo(@PathVariable Integer id, @RequestBody Todo updateTodo){
+    public Todo updateTodo(@PathVariable Integer id, @RequestBody Todo updateTodo) {
         return todoService.updateTodo(id, updateTodo);
     }
 
     @DeleteMapping(path = "/{id}")
-    public void deleteTodo(@PathVariable Integer id){
-         todoService.deleteTodo(id);
+    public void deleteTodo(@PathVariable Integer id) {
+        todoService.deleteTodo(id);
     }
-
 }
