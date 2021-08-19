d7be14db77404f80d382393e4d3dd348851e62c0
Janley Crsiologo <crisoja@oocl.com>
2021-08-19 19:34:21 +0800

    [FrontEnd-BackEnd-IntegrationTest][Janley] refactor: formatted and optimize imports

--- src/main/java/com/example/todolist/service/TodoService.java
+++ src/main/java/com/example/todolist/service/TodoService.java
@@ -3,7 +3,6 @@ package com.example.todolist.service;
 import com.example.todolist.model.Todo;
 import com.example.todolist.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ScopeMetadata;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,37 +13,37 @@ public class TodoService {
     @Autowired
     private TodoRepository todoRepository;
 
-    public TodoService(TodoRepository todoRepository){
+    public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
 
-    public List<Todo> findTodos(){
+    public List<Todo> findTodos() {
         return todoRepository.findAll();
     }
 
-    public Todo addTodo(Todo todo){
+    public Todo addTodo(Todo todo) {
         return todoRepository.save(todo);
     }
 
-    public Todo findTodoById(Integer id ){
+    public Todo findTodoById(Integer id) {
         return todoRepository.findById(id).orElse(null);
     }
 
-    public Todo updateTodo(Integer id, Todo updateTodo){
+    public Todo updateTodo(Integer id, Todo updateTodo) {
         return updateTodoInfo(findTodoById(id), updateTodo);
     }
 
     private Todo updateTodoInfo(Todo todoById, Todo updateTodo) {
-        if(updateTodo.isDone() != null){
+        if (updateTodo.isDone() != null) {
             todoById.setDone(updateTodo.isDone());
         }
-        if(updateTodo.getText() != null){
+        if (updateTodo.getText() != null) {
             todoById.setText(updateTodo.getText());
         }
         return todoRepository.save(todoById);
     }
 
-    public void deleteTodo(Integer id){
+    public void deleteTodo(Integer id) {
         todoRepository.delete(findTodoById(id));
     }
 }
